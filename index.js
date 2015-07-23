785c2346f1847665afd981cfeb562b79d236b834
Jordan Harband <ljharb@gmail.com>
2015-07-23 13:49:54 -0700

    Make sure that *this* module's `.eslintrc` is exported.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var fs = require('fs');
+var path = require('path');
 
-module.exports = JSON.parse(fs.readFileSync('.eslintrc'));
+module.exports = JSON.parse(fs.readFileSync(path.join(__dirname, '.eslintrc')));
