e51633a983b9fb5753fb3107d52db9fb8967da9f
Rouse, Samuel <Samuel.Rouse@bottomline.com>
2019-05-24 19:30:04 -0400

    [Refactor] to `.eslintrc.json`

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
-
-module.exports = JSON.parse(fs.readFileSync(path.join(__dirname, '.eslintrc')));
+module.exports = require('./.eslintrc');
